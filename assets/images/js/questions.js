146b64c4a45294feb9b43956dda3f581f4e96fd2
Abenether <50377779+Abenether@users.noreply.github.com>
2019-09-21 09:38:46 -0400

    3

---
+++ assets/images/js/questions.js
@@ -0,0 +1,141 @@
+const quizQuestions = [
+
+    {
+
+        question: "What is my full name?",
+
+        choices: ["abenether", "abenether Yared", "Abby", "Abenether tadesse"],
+
+        correctAnswer: "Abenether tadesse"
+
+    },
+
+
+
+    {
+
+        question: "What is my favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "basketball"
+
+    },
+
+
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+    {
+
+        question: "What is your favorite sports?",
+
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+
+        correctAnswer: "Soccer"
+
+    },
+
+    
+
+
+
+    {
+
+        question: "What town do you live at?",
+
+        choices: ["OUTSIDE OF US", "ORLANDO", "NJ", "philly"],
+
+        correctAnswer: "PHILLY"
+
+    }
+
+];
+
+
+
+const funImages = [
+
+    './assets/images/happy fun.gif',
+
+    './assets/images/hellYeah.gif',
+
+    './assets/images/winning.gif',
+
+    './assets/images/yeah.gif',
+
+    './assets/images/you did it.gif'
+
+];
+
+
+
+const sadImages = [
+
+    './assets/images/danm.gif',
+
+    './assets/images/oops.gif',
+
+    './assets/images/pretty bad.gif'
+
+];
